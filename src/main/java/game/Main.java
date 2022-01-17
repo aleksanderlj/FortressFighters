d65a84da115a2edfe729b6487b52ab666693e7c4
michaelfn <michaelfognielsen@hotmail.com>
2022-01-17 15:28:32 +0100

    Added shadow to text in launcher

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -104,6 +104,14 @@ public class Main {
 		addComponents(mainPanel);
 		mainPanel.repaint();
 	}
+	
+	private static JButton createButton(String text) {
+		JButton button = new JButton(text);
+		button.setFont(alagard.deriveFont(Font.BOLD, 20));
+		button.setBackground(new Color(219, 180, 132));
+		button.setOpaque(true);
+		return button;
+	}
     
     private static void start(int type) {
 		frame.setVisible(false);
@@ -145,35 +153,34 @@ public class Main {
 			g2D = (Graphics2D) g;
 			if (instructions) {
 				g2D.drawImage(background, (int)((getWidth()/2) - ((background.getWidth()*backgroundScale)/2)), -70, (int)(background.getWidth() * backgroundScale), (int)(background.getHeight()*backgroundScale), null);
+				
 				g2D.setColor(Color.WHITE);
-//				g2D.setFont(alagard.deriveFont(Font.PLAIN, 15));
 				g2D.setFont(new Font("TimesRoman", Font.BOLD, 25));
-				g2D.drawString("How to play:", 20, 40);
-				g2D.drawString("Controls:", 20, 290);
-				
-//				g2D.setFont(alagard.deriveFont(Font.PLAIN, 15));
+				drawStringWithShadow(g2D, "How to play:", 20, 40);
+				drawStringWithShadow(g2D, "Controls:", 20, 290);
+
 				g2D.setFont(new Font("TimesRoman", Font.BOLD, 15));
-				g2D.drawString("- Collect wood and iron and return them to your fortress", 20, 70);
-				g2D.drawString("- Use iron to build cannons that shoot projectiles towards the enemy", 20, 100);
-				g2D.drawString("- Use wood to build walls that stop hostile projectiles", 20, 130);
-				g2D.drawString("- Destroy the enemy fortress with your cannons", 20, 160);
-				g2D.drawString("- Mystical things might happen if you return the orbs on the battle", 20, 190);
-				g2D.drawString("  field to your fortress' orb holders", 20, 210);
+				drawStringWithShadow(g2D, "- Collect wood and iron and return them to your fortress", 20, 70);
+				drawStringWithShadow(g2D, "- Use iron to build cannons that shoot projectiles towards the enemy", 20, 100);
+				drawStringWithShadow(g2D, "- Use wood to build walls that stop hostile projectiles", 20, 130);
+				drawStringWithShadow(g2D, "- Destroy the enemy fortress with your cannons", 20, 160);
+				drawStringWithShadow(g2D, "- Mystical things might happen if you return the orbs on the battle-", 20, 190);
+				drawStringWithShadow(g2D, "  field to your fortress' orb holders", 20, 210);
 				
-				g2D.drawString("- WASD or arrow keys to move", 20, 320);
-				g2D.drawString("- Q to build a cannon", 20, 350);
-				g2D.drawString("- E to build a wall", 20, 380);
+				drawStringWithShadow(g2D, "- WASD or arrow keys to move", 20, 320);
+				drawStringWithShadow(g2D, "- Q to build a cannon", 20, 350);
+				drawStringWithShadow(g2D, "- E to build a wall", 20, 380);
 			} else {
 				g2D.drawImage(background, (int)((getWidth()/2) - ((background.getWidth()*backgroundScale)/2)), -70, (int)(background.getWidth() * backgroundScale), (int)(background.getHeight()*backgroundScale), null);
 				g2D.setColor(Color.WHITE);
 				g2D.setFont(alagard.deriveFont(Font.BOLD, 26));
-				g2D.drawString("Enter your name", 140, 80);
+				drawStringWithShadow(g2D, "Enter your name", 140, 80);
 				g2D.setFont(alagard.deriveFont(Font.BOLD, 35));
-				g2D.drawString("Play Game", 160, 210);
+				drawStringWithShadow(g2D, "Play Game", 160, 210);
 				g2D.setFont(alagard.deriveFont(Font.BOLD, 12));
-				g2D.drawString("Enter IP Address to join:", 270, 305);
-				g2D.setColor(Color.BLACK);
+				drawStringWithShadow(g2D, "Enter IP Address to join:", 270, 305);
 			}
+			g2D.setColor(Color.BLACK);
 		}
 		
 		@Override
@@ -190,12 +197,12 @@ public class Main {
 
 		@Override
 		public void keyReleased(KeyEvent e) {}
-	}
-    
-	private static JButton createButton(String text) {
-		JButton button = new JButton(text);
-		button.setFont(alagard.deriveFont(Font.BOLD, 20));
-		button.setBackground(new Color(219, 180, 132));
-		return button;
+		
+		public void drawStringWithShadow(Graphics2D g2D, String text, int x, int y) {
+			g2D.setColor(Color.BLACK);
+			g2D.drawString(text, x+3, y+2);
+			g2D.setColor(Color.WHITE);
+			g2D.drawString(text, x, y);
+		}
 	}
 }
