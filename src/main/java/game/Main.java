ed3c3304bd459a6e0ccf437d9dd00b99f1bc2207
michaelfn <michaelfognielsen@hotmail.com>
2022-01-17 16:50:19 +0100

    Adjusted shadows of text in launcher

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -194,7 +194,7 @@ public class Main {
 		
 		public void drawStringWithShadow(Graphics2D g2D, String text, int x, int y) {
 			g2D.setColor(Color.BLACK);
-			g2D.drawString(text, x+3, y+2);
+			g2D.drawString(text, x+2, y+2);
 			g2D.setColor(Color.WHITE);
 			g2D.drawString(text, x, y);
 		}
